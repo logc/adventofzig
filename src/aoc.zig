953aca05615aaf0bfd13e267eb29a5b5d3802e30
Luis Osa <luis.osa.gdc@gmail.com>
2023-10-15 15:56:16 +0200

    I was told there would be no math

--- src/aoc.zig
+++ src/aoc.zig
@@ -1,7 +1,68 @@
 const std = @import("std");
+
 const expect = std.testing.expect;
 
-const Solutions = struct { first: i32, second: i32 };
+pub const Solutions = struct { first: i32, second: i32 };
+
+pub fn iWasToldThereWouldBeNoMath(input: []const u8) Solutions {
+    const Dimensions = struct { l: i32, w: i32, h: i32 };
+    var lines = std.mem.split(u8, input, "\n");
+    var paper: i32 = 0;
+    var ribbon: i32 = 0;
+    while (lines.next()) |line| {
+        var tokens = std.mem.split(u8, line, "x");
+        var dims = Dimensions{ .l = 0, .w = 0, .h = 0 };
+        var idx: u8 = 0;
+        while (tokens.next()) |token| : (idx += 1) {
+            const dim = std.fmt.parseInt(i32, token, 10) catch 0;
+            if (idx == 0) dims.l = dim;
+            if (idx == 1) dims.w = dim;
+            if (idx == 2) dims.h = dim;
+        }
+        const min_side = min(dims.l * dims.w, dims.w * dims.h, dims.h * dims.l);
+        paper += (2 * dims.l * dims.w + 2 * dims.w * dims.h + 2 * dims.h * dims.l);
+        paper += min_side;
+        ribbon += min_perimeter(dims.l, dims.w, dims.h);
+        ribbon += dims.l * dims.w * dims.h;
+    }
+    return Solutions{ .first = paper, .second = ribbon };
+}
+
+fn min_perimeter(a: i32, b: i32, c: i32) i32 {
+    var a_b = a * 2 + b * 2;
+    var b_c = b * 2 + c * 2;
+    var a_c = a * 2 + c * 2;
+    return min(a_b, b_c, a_c);
+}
+
+fn min(a: i32, b: i32, c: i32) i32 {
+    var min_val = a;
+    if (b < min_val) {
+        min_val = b;
+    }
+    if (c < min_val) {
+        min_val = c;
+    }
+    return min_val;
+}
+
+test "min" {
+    try expect(min(1, 2, 3) == 1);
+    try expect(min(1, 1, 1) == 1);
+    try expect(min(3, 2, 1) == 1);
+    try expect(min(3, 1, 2) == 1);
+}
+
+test "I Was Told There Would Be No Math" {
+    const dims1 = "2x3x4\n";
+    try expect(iWasToldThereWouldBeNoMath(dims1).first == (2 * 6 + 2 * 12 + 2 * 8) + 6);
+
+    const dims2 = "1x1x10\n";
+    try expect(iWasToldThereWouldBeNoMath(dims2).first == (2 * 1 + 2 * 10 + 2 * 10) + 1);
+
+    const dims3 = "2x3x4\n1x1x10";
+    try expect(iWasToldThereWouldBeNoMath(dims3).first == (58 + 43));
+}
 
 pub fn notQuiteLisp(directions: []const u8) Solutions {
     var floor: i32 = 0;
--- src/main.zig
+++ src/main.zig
@@ -12,14 +12,22 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
-    const cliArgs = try readCli(alloc);
-    var puzzleInput = try readPuzzleInput(alloc, cliArgs.year, cliArgs.day);
-    var solutions = aoc.notQuiteLisp(puzzleInput);
+    const cliArgs = try readCli();
+    var puzzleInput = readPuzzleInput(alloc, cliArgs.year, cliArgs.day) catch "";
+    defer alloc.free(puzzleInput);
+    var solutions = aoc.Solutions{ .first = 0, .second = 0 };
+    if (mem.eql(u8, cliArgs.year, "2015")) {
+        if (mem.eql(u8, cliArgs.day, "01")) {
+            solutions = aoc.notQuiteLisp(puzzleInput);
+        }
+        if (mem.eql(u8, cliArgs.day, "02")) {
+            solutions = aoc.iWasToldThereWouldBeNoMath(puzzleInput);
+        }
+    }
     std.debug.print("Puzzle 1: {d}\nPuzzle 2: {d}\n", .{ solutions.first, solutions.second });
 }
 
-fn readCli(alloc: mem.Allocator) !CliArgs {
-    _ = alloc;
+fn readCli() !CliArgs {
     const params = comptime clap.parseParamsComptime(
         \\-y, --year <str>   Year of advent problem to solve.
         \\-d, --day <str>    Day within year to solve.
@@ -42,13 +50,17 @@ fn readCli(alloc: mem.Allocator) !CliArgs {
 }
 
 fn readPuzzleInput(alloc: mem.Allocator, year: ?[]const u8, day: ?[]const u8) ![]const u8 {
-    const filename = try std.fmt.allocPrint(alloc, "var/data/{?s}/{?s}/input.txt", .{ year, day });
+    const nameTemp = "var/data/{?s}/{?s}/input.txt";
+    const filename = try std.fmt.allocPrint(alloc, nameTemp, .{ year, day });
     defer alloc.free(filename);
-    std.debug.print("Opening: {s}\n", .{filename});
-    const file = try std.fs.cwd().openFile(filename, .{});
+    std.debug.print("Opening file: {s}\n", .{filename});
+    const file = std.fs.cwd().openFile(filename, .{}) catch |err| {
+        if (err == error.FileNotFound) {
+            std.debug.print("{s} not found\n", .{filename});
+            return err;
+        }
+        return err;
+    };
     defer file.close();
-    var buf: [10000]u8 = undefined;
-    const read = try file.read(&buf);
-    _ = read;
-    return &buf;
+    return file.readToEndAlloc(alloc, 1024 * 1024);
 }
